bbe631003ee8ea6a6752eab700b28c6e23e62fd8
Justin Grosvenor <justingrosvenor@gmail.com>
2026-06-09 21:29:31 -0700

    Ignore dist/ in eslint; ignore local .npmrc

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["node_modules/", "*.config.*"] },
+  { ignores: ["node_modules/", "dist/", "*.config.*"] },
   js.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
